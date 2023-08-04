6829e8102b1714947aabdc27f29adcce3f471d31
Jaimin78 <jaiminsuthar7802@gmail.com>
2023-08-04 15:43:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const connectDb = require('./db');
 const express = require('express');
+const path = require('path');
 connectDb();
 const app = express()
 const cors = require('cors')
@@ -7,6 +8,9 @@ const cors = require('cors')
 app.use(cors());
 
 app.use(express.json());
+
+// Serve static files from the 'uploads' directory
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 //Routes
 app.use('/api/user', require('./routes/user'));
