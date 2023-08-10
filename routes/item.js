f735020f63085d69f5ef97077f6cf3d973e2c124
Jaimin78 <jaiminsuthar7802@gmail.com>
2023-08-10 15:40:59 +0530

    created a route for order status and past orders

--- routes/item.js
+++ routes/item.js
@@ -47,14 +47,44 @@ app.get('/get', tokenVerification, async (req, res) => {
 //Order item : api/item/order 
 app.post('/order', tokenVerification, async (req, res) => {
     try {
-        console.log(req.body.customerName)
         const newOrder = new Order(req.body);
         await newOrder.save();
         await newOrder.save();
         res.status(201).json({ success: true, message: 'Order created successfully' });
     }
     catch (error) {
-        res.status(500).json({ success: false, message: 'Failed to order items', error: error.message })
+        res.status(500).json({ success: false, message: 'Internal Server Error', error: error.message })
     }
 })
+
+//Get Order :  api/item/order/get
+app.post('/order/get', tokenVerification, async (req, res) => {
+    try{
+        let customerid = req.headers['customerid'];
+        const order = await Order.find({
+            customerid: customerid,
+            status: { $in: ["Preparing", "Ready"] }
+        });
+        res.status(200).send(order)
+    }
+    catch(error){
+        res.status(500).json({ success: false, message: 'Internal Server Error', error: error.message })
+    }
+})
+
+//Get Past Order :  api/item/order/past
+app.post('/order/past', tokenVerification, async (req, res) => {
+    try{
+        let customerid = req.headers['customerid'];
+        const order = await Order.find({
+            customerid: customerid,
+            status: "Past"
+        });
+        res.status(200).send(order)
+    }
+    catch(error){
+        res.status(500).json({ success: false, message: 'Internal Server Error', error: error.message })
+    }
+})
+
 module.exports = app;
